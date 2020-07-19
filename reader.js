f649a52e290a658b165e1915da5f7f5c02bc010b
Alija1 <58340674+Alija1@users.noreply.github.com>
2020-07-19 22:05:16 +0200

    Add files via upload

---
+++ reader.js
@@ -0,0 +1,45 @@
+let button = document.getElementById("get-text-btn");
+            let textArea = document.getElementById("my-text-area");
+            let loader = document.getElementById("loader");
+            button.addEventListener("click", function () {
+                getData();
+            });
+            async function getData() {
+                try {
+                    loader.style.display = "inline-block";
+                    let response = await fetch('https://v-dresevic.github.io/Advanced-JavaScript-Programming/data/students.txt');
+                    if (response.status !== 200) {
+                        throw new Error("Error while reading file.");
+                    }
+                    let text = await response.text();
+                    let studentList = text.split("\n");
+                    class Student {
+
+                        constructor(name, address, phone, course){
+                            this.name = name;
+                            this.address = address;
+                            this.phone = phone;
+                            this.course = course;
+                        }
+                    
+                        getInfo() {
+                            return "Name: " + this.name + "\n" +
+                            "Address: " + this.address + "\n" +
+                            "Phone: " + this.phone + "\n" +
+                            "Course: " + this.course
+                        }
+                    }
+                    var newStudents = [];
+                    while (text.length > 0) {
+                        var newStudent = new Student(text[0], text[1], text[2], text[3]);
+                        newStudents.push(newStudent.getInfo() + '\n');
+                        studentList.splice(0, 4);
+                        textArea.innerHTML = newStudent;
+                    }
+                } catch (err) {
+                    textArea.innerHTML = 'Fetch problem: ' + err.message;
+                } finally {
+                    loader.style.display = "none";
+                }
+            }
+
