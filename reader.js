6b518985d444b6027adb7f2ce672b3a28d6d09c5
Alija1 <58340674+Alija1@users.noreply.github.com>
2020-07-19 22:00:51 +0200

    Update reader.js

--- reader.js
+++ reader.js
@@ -1,20 +1,3 @@
-class Student {
-
-    constructor(name, address, phone, course){
-        this.name = name;
-        this.address = address;
-        this.phone = phone;
-        this.course = course;
-    }
-
-    getInfo() {
-        return "Name: " + this.name + "\n" +
-        "Address: " + this.address + "\n" +
-        "Phone: " + this.phone + "\n" +
-        "Course: " + this.course
-    }
-}
-
 let button = document.getElementById("get-text-btn");
             let textArea = document.getElementById("my-text-area");
             let loader = document.getElementById("loader");
